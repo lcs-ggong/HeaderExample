d86e9c0515fc729ae3d4a8079e489bf189063824
Grace Gong <ggong@lcs.on.ca>
2022-11-07 11:33:21 -0500

    Initial Commit

---
+++ HeaderExample/HeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExampleApp.swift
+//  HeaderExample
+//
+//  Created by Grace Gong on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
